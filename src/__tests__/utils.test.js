4bd295f6a522a73f3f8871672a9a15e763c379ec
Imelda Hope <imeldalarusso@gmail.com>
2024-08-08 19:06:15 +0300

    adds more test suites

--- src/__tests__/utils.test.js
+++ src/__tests__/utils.test.js
@@ -2,9 +2,27 @@ import {isPalindrome} from '../utils'
 
 describe('isPalindrome', () => {
     it('gives double points for words reading the same forward and backwards (e.g racecar) and 1 point otherwise', () => {
-        const palindrome = 'car';
+        const palindrome = 'racecar';
         const points = isPalindrome(palindrome);
-
-        expect(points).toBe(1);
-    })
-})
+        expect(points).toBe(2);
+    }),
+    it('checks for case-sensitivity and alphanumeric characters', () => {
+        const palindrome = 'A man, a plan, a canal, Panama';
+        const points = isPalindrome(palindrome);
+        expect(points).toBe(2);
+    }),
+    it('returns false for an empty string', () => {
+        const palindrome = '';
+        const points = isPalindrome(palindrome);
+        expect(points).toBe(false);
+    }),
+    it('return true for words that are a combination of uppercase and lowercase letters', () => {
+        const palindrome = 'MaDam';
+        const points = isPalindrome(palindrome);
+        expect(points).toBe(2);
+    }),
+    it('should throw an error if input has any non-alphabetic characters', () => {
+        const palindrome = 'racer23';        
+        expect(() => isPalindrome(palindrome)).toThrow("Input contains non-alphabetic characters");
+    });
+});
--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,17 @@
 
 export function isPalindrome(palindromeWord){
-    const points = palindromeWord === palindromeWord.split('').reverse().join('')? 2 : 1;
     
-    return points;
+    //.replace() replaces all non-alphanumeric characters in the string with an empty string
+    // /g flag tells the .replace() method to search for all matches in the string, not just the first one
+    //clean up string
+    const word = palindromeWord.replace(/[^a-zA-Z]/g, '').toLowerCase();
+    const reversedWord = word.split('').reverse().join('');
+
+    if(word === '') return false;  
+
+    if (/[^a-zA-Z\s]+$/.test(palindromeWord)) {
+        throw new Error("Input contains non-alphabetic characters");
+    }
+
+    return word === reversedWord ? 2 : 1;       
 }
